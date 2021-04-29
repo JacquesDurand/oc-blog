f16ce662b4ecf0acfd9aba9b821c27638e4d2bbb
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2021-04-29 18:38:08 +0200

    [unissued chore]: start docker setup

---
+++ data.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE dockerSample (
+    name varchar(20) NULL
+);
+
+CREATE TABLE toto (
+    id INT PRIMARY KEY NOT NULL
+);
+
+INSERT INTO dockerSample VALUES ('George'),('Sam'),('Kathy');
+
+INSERT INTO toto VALUES (1);
+
