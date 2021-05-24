8e558967d1fe13185472a9a3fe189e9bc7533673
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2021-05-24 10:04:51 +0200

    [feature 3]: create Models

--- public/index.php
+++ public/index.php
@@ -1,22 +1,28 @@
 <?php
 
 use App\Manager\CategoryManager;
+use App\Model\Category;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$value = "World";
+$value = "Toto";
 
 $managerTest = new CategoryManager();
 $managerTest->createCategory('category4');
-$databaseTest = $managerTest->getAllCategories();
-
+$categories = $managerTest->getAllCategories();
+foreach ($categories as $category) {
+    $categoryModel = new Category();
+    $categoryModel->setId($category->id);
+    $categoryModel->setName($category->name);
+}
+var_dump($categoryModel);
 ?>
 
 <html>
 <body>
 <h1>Hello, <?= $value ?>!</h1>
 
-<?php foreach($databaseTest as $row): ?>
+<?php foreach($categories as $row): ?>
     <p>Hello, <?= $row->name ?></p>
 <?php endforeach; ?>
 </body>
---
+++ src/Model/User.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Model;
+
+
+use DateTime;
+
+class User
+{
+    /** @var int */
+    private $id;
+
+    /** @var string */
+    private $userName;
+
+    /** @var string */
+    private $password;
+
+    /** @var string */
+    private $email;
+
+    /** @var int */
+    private $role;
+
+    /** @var DateTime */
+    private $createdAt;
+
+    /** @var DateTime */
+    private $updatedAt;
+
+    /** @var DateTime */
+    private $lastActivity;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserName(): string
+    {
+        return $this->userName;
+    }
+
+    /**
+     * @param string $userName
+     */
+    public function setUserName(string $userName): void
+    {
+        $this->userName = $userName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return int
+     */
+    public function getRole(): int
+    {
+        return $this->role;
+    }
+
+    /**
+     * @param int $role
+     */
+    public function setRole(int $role): void
+    {
+        $this->role = $role;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime $updatedAt
+     */
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getLastActivity(): DateTime
+    {
+        return $this->lastActivity;
+    }
+
+    /**
+     * @param DateTime $lastActivity
+     */
+    public function setLastActivity(DateTime $lastActivity): void
+    {
+        $this->lastActivity = $lastActivity;
+    }
+
+}
