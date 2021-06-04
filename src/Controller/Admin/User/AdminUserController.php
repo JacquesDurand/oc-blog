7db829222c8397e49ce958ceb6b8e53aa91dd8b0
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2021-06-04 09:06:54 +0200

    [feature 25]:Basic Admin user controller/manager + templates

--- src/Controller/Admin/User/AdminUserController.php
+++ src/Controller/Admin/User/AdminUserController.php
@@ -4,39 +4,89 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin\User;
 
+use App\Controller\Twig\AbstractController;
+use App\Exception\ResourceNotFoundException;
 use App\HTTP\Request;
 use App\Manager\UserManager;
+use App\Model\User;
 
 require_once __DIR__.'/../../../../vendor/autoload.php';
 
 
-class AdminUserController
+class AdminUserController extends AbstractController
 {
     /** @var UserManager */
     private $userManager;
 
     public function __construct()
     {
+        parent::__construct();
         $this->userManager = new UserManager();
     }
 
     public function getAllUsers(Request $request)
     {
+        $users = $this->userManager->getAllUsers();
+        if ($users) {
+            echo $this->render('/Admin/User/show.html.twig', ['users' => $users]);
+        } else {
+            echo $this->render('/Admin/User/show.html.twig');
+        }
     }
 
     public function addUser(Request $request)
     {
+        ##TODO crypt password + verify data
+        switch ($request->method) {
+            case 'GET':
+                echo $this->render('Admin/User/form.html.twig');
+                break;
+            case 'POST':
+                $this->userManager->createUser($request->request);
+                header("Location: http://localhost/admin/users");
+        }
     }
 
     public function getUserById(Request $request)
     {
+        $id = (int)$request->requirements[0];
+        if ($user = $this->userManager->getUserById($id)) {
+            echo $this->render('/Admin/User/show_one.html.twig', ['user' => $user]);
+        }
     }
 
     public function deleteUser(Request $request)
     {
+        $id = (int)$request->requirements[0];
+        try {
+            $this->userManager->deleteUser($id);
+            echo $this->render('/Admin/User/show.html.twig');
+        } catch (ResourceNotFoundException $exception) {
+            echo $this->render('Errors/404_resource.html.twig');
+        }
     }
 
     public function updateUser(Request $request)
     {
+        $id = (int) $request->requirements[0];
+
+        switch ($request->method) {
+            case 'GET':
+                if ($user = $this->userManager->getUserById($id)) {
+                    echo $this->render('/Admin/User/update.html.twig', [
+                        'user' => $user
+                    ]);
+                } else {
+                    echo $this->render('Errors/404_resource.html.twig');
+                }
+                break;
+            case 'POST':
+                try {
+                    $this->userManager->updateUser($id, $request->request);
+                    header("Location: http://localhost/admin/users");
+                } catch (ResourceNotFoundException $exception) {
+                    echo $this->render('Errors/404_resource.html.twig');
+                }
+        }
     }
 }
--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -6,6 +6,7 @@ namespace App\Manager;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
+use App\Exception\ResourceNotFoundException;
 use App\Traits\DbInstanceTrait;
 use PDO;
 
@@ -17,6 +18,64 @@ class UserManager
     {
         $req = $this->dbInstance->prepare('SELECT * FROM users');
         $req->execute();
-        return $req->fetchAll(PDO::FETCH_OBJ);
+        $req->setFetchMode(PDO::FETCH_CLASS, 'User');
+        return $req->fetchAll();
+    }
+
+    public function getUserById(int $id)
+    {
+        $req = $this->dbInstance->prepare('SELECT * FROM users WHERE id=:id');
+        $req->bindValue(':id', $id);
+        $req->execute();
+        $req->setFetchMode(PDO::FETCH_CLASS, 'User');
+        return $req->fetch();
+    }
+
+    public function createUser(array $data): void
+    {
+        $req = $this->dbInstance->prepare(
+            'INSERT INTO users(username, password, email, role) 
+                   VALUES (:username, :password, :email, :role)'
+        );
+        $req->bindValue(':username', $data['username']);
+        $req->bindValue(':password', $data['password']);
+        $req->bindValue(':email', $data['email']);
+        $req->bindValue(':role', $data['role']);
+        $req->execute();
+    }
+
+    /**
+     * @throws ResourceNotFoundException
+     */
+    public function deleteUser(int $id): void
+    {
+        if ($this->getUserById($id)) {
+            $req = $this->dbInstance->prepare('DELETE FROM users WHERE id=:id');
+            $req->bindValue(':id', $id);
+            $req->execute();
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    /**
+     * @throws ResourceNotFoundException
+     */
+    public function updateUser(int $id, array $data): void
+    {
+        if ($this->getUserById($id)) {
+            $req = $this->dbInstance->prepare(
+                'UPDATE users SET username=:username, email=:email, password=:password, role=:role
+                     WHERE id=:id'
+            );
+            $req->bindValue(':username', $data['username']);
+            $req->bindValue(':password', $data['password']);
+            $req->bindValue(':email', $data['email']);
+            $req->bindValue(':role', $data['role']);
+            $req->bindValue(':id', $id);
+            $req->execute();
+        } else {
+            throw new ResourceNotFoundException();
+        }
     }
 }
