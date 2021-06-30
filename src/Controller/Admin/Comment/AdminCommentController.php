7a9b7f9b5d748d47634db820bce99209b7b6eea9
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2021-06-30 21:33:07 +0200

    [feature 36]: admin comment controller/manager/init views

--- src/Controller/Admin/Comment/AdminCommentController.php
+++ src/Controller/Admin/Comment/AdminCommentController.php
@@ -6,32 +6,169 @@ namespace App\Controller\Admin\Comment;
 
 require_once __DIR__.'/../../../../vendor/autoload.php';
 
+use App\Controller\Twig\AbstractController;
+use App\Exception\ResourceNotFoundException;
 use App\HTTP\Request;
 use App\Manager\CommentManager;
+use App\Manager\PostManager;
+use App\Manager\UserManager;
+use App\Model\Comment;
+use PDOException;
 
-class AdminCommentController
+class AdminCommentController extends AbstractController
 {
     /** @var CommentManager */
-    private $commentManager;
+    private CommentManager $commentManager;
+
+    /** @var PostManager */
+    private PostManager $postManager;
+
+    /** @var UserManager */
+    private UserManager $userManager;
 
     public function __construct()
     {
+        parent::__construct();
         $this->commentManager = new CommentManager();
+        $this->postManager = new PostManager();
+        $this->userManager = new UserManager();
     }
 
     public function getAllComments(Request $request)
     {
+        $comments = $this->commentManager->getAllComments();
+        echo $this->render('Admin/Comment/show.html.twig', [
+            'comments' => $comments
+        ]);
     }
 
     public function getCommentById(Request $request)
     {
+        $id = (int) $request->requirements[0];
+        if ($comment = $this->commentManager->getCommentById($id)) {
+            echo $this->render('Admin/Comment/show_one.html.twig', [
+                'comment' => $comment
+            ]);
+        } else {
+            echo $this->render('Errors/404_resource.html.twig');
+        }
+    }
+
+    public function addComment(Request $request)
+    {
+        $id = (int) $request->requirements[0];
+        switch ($request->method) {
+            case 'GET':
+                if ($post = $this->postManager->getPostById($id)) {
+                    $this->generateCsrfToken($request);
+                    echo $this->render('Admin/Comment/form.html.twig', [
+                       'post' => $post,
+                       'token' => $_SESSION['csrf_token']
+                    ]);
+                } else {
+                    echo $this->render('Errors/404_resource.html.twig');
+                }
+                break;
+            case 'POST':
+                if (!$this->verifyCsrfToken($request)) {
+                    echo $this->render('Errors/Csrf.html.twig');
+                } else {
+                    $this->cleanInput($request);
+                    $comment = $this->hydrateCommentFromRequest($request);
+                    try {
+                        $this->commentManager->addCommentToPost($comment);
+                        header('Location: http://localhost/admin/comments');
+                    } catch (PDOException $exception) {
+                        var_dump($exception->getMessage());
+                    }
+                }
+                break;
+        }
+    }
+
+    public function updateComment(Request $request)
+    {
+        $id = (int) $request->requirements[0];
+        switch ($request->method) {
+            case 'GET':
+                if ($comment = $this->commentManager->getCommentById($id)) {
+                    $this->generateCsrfToken($request);
+                    echo $this->render('Admin/Comment/update.html.twig', [
+                       'comment' => $comment,
+                       'token' => $_SESSION['csrf_token']
+                    ]);
+                } else {
+                    echo $this->render('Errors/404_resource.html.twig');
+                }
+                break;
+            case 'POST':
+                if (!$this->verifyCsrfToken($request)) {
+                    echo $this->render('Errors/Csrf.html.twig');
+                } else {
+                    $this->cleanInput($request);
+                    $comment = $this->hydrateCommentFromRequest($request);
+                    $comment->setId($id);
+                    try {
+                        $this->commentManager->updateComment($comment);
+                        header('Location: http://localhost/admin/comments');
+                    } catch (ResourceNotFoundException $exception) {
+                        echo $this->render('Errors/404_resource.html.twig');
+                    }
+                }
+        }
     }
 
     public function moderate(Request $request)
     {
+        $id = (int) $request->requirements[0];
+        switch ($request->method) {
+            case 'GET':
+                if ($comment = $this->commentManager->getCommentById($id)) {
+                    $this->generateCsrfToken($request);
+                    echo $this->render('Admin/Comment/moderate.html.twig', [
+                        'comment' => $comment,
+                        'token' => $_SESSION['csrf_token']
+                    ]);
+                } else {
+                    echo $this->render('Errors/404_resource.html.twig');
+                }
+                break;
+            case 'POST':
+                if (!$this->verifyCsrfToken($request)) {
+                    echo $this->render('Errors/Csrf.html.twig');
+                } else {
+                    try {
+                        $this->cleanInput($request);
+                        $this->commentManager->moderateComment($id, $request->request['moderationReason']);
+                        header('Location: http://localhost/admin/comments');
+                    } catch (ResourceNotFoundException $exception) {
+                        echo $this->render('Errors/404_resource.html.twig');
+                    }
+                }
+        }
     }
 
     public function approve(Request $request)
     {
+        $id = (int) $request->requirements[0];
+        try {
+            $this->commentManager->approveComment($id);
+            header('Location: http://localhost/admin/comments');
+        } catch (ResourceNotFoundException $exception) {
+            echo $this->render('Errors/404_resource.html.twig');
+        }
+    }
+
+    private function hydrateCommentFromRequest(Request $request): Comment
+    {
+        $comment = new Comment();
+
+        $comment->setContent($request->request['content']);
+        $comment->setModerationReason($request->request['moderationReason']);
+        $comment->setState(Comment::STATE_VALIDATED);
+        $comment->setAuthor($this->userManager->getUserById((int) $request->request['author']));
+        $comment->setPost($this->postManager->getPostById((int) $request->requirements[0]));
+
+        return $comment;
     }
 }
--- src/Manager/CommentManager.php
+++ src/Manager/CommentManager.php
@@ -6,22 +6,150 @@ namespace App\Manager;
 
 require_once __DIR__.'/../../vendor/autoload.php';
 
+use App\Exception\ResourceNotFoundException;
+use App\HTTP\Request;
+use App\Model\Comment;
 use App\Traits\DbInstanceTrait;
 use PDO;
+use PDOException;
 
 class CommentManager
 {
     use DbInstanceTrait;
 
+    /** @var UserManager */
+    private UserManager $userManager;
+
+    /** @var PostManager */
+    private PostManager $postManager;
+
     public function __construct()
     {
         $this->connect();
+        $this->postManager = new PostManager();
+        $this->userManager = new UserManager();
     }
 
     public function getAllComments(): array
     {
         $req = $this->dbInstance->prepare('SELECT * FROM comment');
         $req->execute();
-        return $req->fetchAll(PDO::FETCH_OBJ);
+        return $this->hydrateCommentsWithFk($req->fetchAll());
+    }
+
+    public function getCommentById(int $id)
+    {
+        $req = $this->dbInstance->prepare('SELECT * FROM comment WHERE id=:id');
+        $req->bindValue(':id', $id);
+        $req->execute();
+        if ($result = $req->fetch()) {
+            return $this->hydrateCommentWithFk($result);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param int $id
+     * @param string $moderationReason
+     * @throws ResourceNotFoundException
+     */
+    public function moderateComment(int $id, string $moderationReason)
+    {
+        if ($this->getCommentById($id)) {
+            $req = $this->dbInstance->prepare(
+                'UPDATE comment 
+                   SET moderation_reason = :moderationReason, state = :state
+                   WHERE id =:id'
+            );
+            $req->bindValue(':moderationReason', $moderationReason);
+            $req->bindValue(':state', Comment::STATE_MODERATED);
+            $req->bindValue(':id', $id);
+            $req->execute();
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    /**
+     * @param Comment $comment
+     * @throws PDOException
+     */
+    public function addCommentToPost(Comment $comment)
+    {
+        $req = $this->dbInstance->prepare(
+            'INSERT INTO comment(content, state, moderation_reason, author_id, post_id)
+                VALUES (:content, :state, :moderationReason, :author, :post)'
+        );
+        $req->bindValue(':content', $comment->getContent());
+        $req->bindValue(':state', $comment->getState());
+        $req->bindValue(':moderationReason', $comment->getModerationReason());
+        $req->bindValue(':author', $comment->getAuthor()->getId());
+        $req->bindValue(':post', $comment->getPost()->getId());
+
+        $req->execute();
+    }
+
+    public function updateComment(Comment $comment)
+    {
+        $req = $this->dbInstance->prepare(
+            'UPDATE comment
+                   SET content = :content,
+                   state = :state,
+                   moderation_reason = :moderationReason,
+                   author_id = :author,
+                   post_id = :post
+                   WHERE id = :id'
+        );
+        $req->bindValue(':content', $comment->getContent());
+        $req->bindValue(':state', $comment->getState());
+        $req->bindValue(':moderationReason', $comment->getModerationReason());
+        $req->bindValue(':author', $comment->getAuthor()->getId());
+        $req->bindValue(':post', $comment->getPost()->getId());
+        $req->bindValue(':id', $comment->getId());
+
+        $req->execute();
+    }
+
+    /**
+     * @param int $id
+     * @throws ResourceNotFoundException
+     */
+    public function approveComment(int $id)
+    {
+        if ($this->getCommentById($id)) {
+            $req = $this->dbInstance->prepare('UPDATE comment SET state = :state WHERE id = :id');
+            $req->bindValue(':state', Comment::STATE_VALIDATED);
+            $req->bindValue('id', $id);
+            $req->execute();
+        } else {
+            throw new ResourceNotFoundException();
+        }
+    }
+
+    private function hydrateCommentsWithFk(array $dbComments): array
+    {
+        $comments = [];
+
+        foreach ($dbComments as $dbComment) {
+            $comments[] = $this->hydrateCommentWithFk($dbComment);
+        }
+        return $comments;
+    }
+
+    private function hydrateCommentWithFk(array $dbComment): Comment
+    {
+        $comment = new Comment();
+
+        $comment->setId($dbComment['id']);
+        $comment->setState($dbComment['state']);
+        $comment->setContent($dbComment['content']);
+        $comment->setModerationReason($dbComment['moderation_reason']);
+        $comment->setCreatedAt(new \DateTime($dbComment['created_at']));
+        $comment->setUpdatedAt(new \DateTime($dbComment['updated_at']));
+        $comment->setAuthor($this->userManager->getUserById($dbComment['author_id']));
+        $comment->setPost($this->postManager->getPostById($dbComment['post_id']));
+
+        return $comment;
     }
 }
