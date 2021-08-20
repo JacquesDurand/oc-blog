b50ebd8b463b7f8e172e23914410596cab33dba3
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2021-08-20 15:00:05 +0200

    [feature 1]: homepage

--- public/index.php
+++ public/index.php
@@ -6,24 +6,8 @@ use App\Model\Category;
 use App\Routing\Router;
 
 require_once __DIR__.'/../vendor/autoload.php';
-//require __DIR__.'/../src/templates/Admin/Category/show.html.twig';
 
-$value = "Toto";
-
-//$managerTest = new CategoryManager();
-//$managerTest->createCategory('category4');
-//$categories = $managerTest->getAllCategories();
-//foreach ($categories as $category) {
-//    $categoryModel = new Category();
-//    $categoryModel->setId($category->id);
-//    $categoryModel->setName($category->name);
-//}
 $request = Request::createFromGlobals();
 $router = new Router();
 $router->handleRequest($request);
 
-//$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../src/templates');
-//$twig = new \Twig\Environment($loader, ['debug' => true]);
-//
-////$twig->load('Admin/Category/show.html.twig');
-//$twig->render('Admin/Category/show.html.twig', ['categories' => 'toto'] );
